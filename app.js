5ee586b0b31a18a1f2e7ef881a987cce8a063f83
VolodymyrKucheruk <voloda.2014a@gmail.com>
2024-04-18 23:27:32 +0200

    mod

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", router);
+app.use("/api/users", router);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
